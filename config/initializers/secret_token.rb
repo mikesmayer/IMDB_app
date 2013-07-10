77072d97539b42dab9e6103d374e9647d75f594c
Mike Mayer <mayerms@gmail.com>
2013-07-10 15:39:42 -0400

    rails new IMDB_app and added dev gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMDBApp::Application.config.secret_token = 'd20aa8ce245866262deef04a6bc238a017584a1bf8f13b376a9b9967ccfebe269bfd209d57610d17a0437387b9e7c6590cd608b60e235cf98aa115f67dc8fa08'
